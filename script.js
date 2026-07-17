567ee279b022ae2b896070b4e8b9739629461c64
Nicole (Niki) Golen <47008141+NikiGolen@users.noreply.github.com>
2026-07-16 20:45:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -68,3 +68,22 @@ showSlides();
 sections.forEach((section) => {
   if (section.id) observer.observe(section);
 });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  // Hide all slides
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  // Increment index and loop back to the start
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  
+  // Display the current slide
+  slides[slideIndex-1].style.display = "block";  
+  
+  // Change image every 3 seconds
+  setTimeout(showSlides, 3000); 
+}
